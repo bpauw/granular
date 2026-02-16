a91091784e4489d99a65d7c9720b037ec9941dc2
bpauw <spam@pauw.ca>
2026-02-16 07:43:52 -0700

    remove custom view help text

--- src/granular/terminal/custom_view.py
+++ src/granular/terminal/custom_view.py
@@ -7,5 +7,4 @@ from granular.terminal.custom_typer import AlphabeticalContextAwareGroup
 app = typer.Typer(
     cls=AlphabeticalContextAwareGroup,
     no_args_is_help=True,
-    help="User-defined custom views",
 )
